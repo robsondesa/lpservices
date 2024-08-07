b076b4cfa4aae0bc5919d3fdcf7a1860a959b63c
Robson de Sá <89641890+robsondesa@users.noreply.github.com>
2024-08-07 13:01:34 +0000

    att

--- js/main.js
+++ js/main.js
@@ -86,6 +86,25 @@
         }
     });
 
+      /**
+   * Init swiper sliders
+   */
+    function initSwiper() {
+        document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
+        let config = JSON.parse(
+            swiperElement.querySelector(".swiper-config").innerHTML.trim()
+        );
+
+        if (swiperElement.classList.contains("swiper-tab")) {
+            initSwiperWithCustomPagination(swiperElement, config);
+        } else {
+            new Swiper(swiperElement, config);
+        }
+        });
+    }
+
+    window.addEventListener("load", initSwiper);
+
     /*--------------------
         Gallery Items Animation
     --------------------*/
